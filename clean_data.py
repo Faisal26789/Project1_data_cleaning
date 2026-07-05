38ccb12137dec7a03ec8f7ccb8973a07aa45f714
Faisal26789 <fs6881298@gmail.com>
2026-07-05 21:11:27 +0530

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -4,7 +4,7 @@ Project 1: Data Cleaning & Preparation
 Goal: Clean a raw e-commerce "Online Store Orders" dataset by handling
 missing values, duplicates, and incorrect/inconsistent data.
 
-Author: [Your Name]
+Author: Md. Faisal Siddiqui 
 Dataset: Online-Store-Orders.xlsx
 """
 
